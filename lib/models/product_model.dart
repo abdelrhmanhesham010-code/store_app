4e1c600d3483542de0d286ed30561bfa1cc630b2
abod_hesahm <egalsclub5@gmail.com>
2025-12-28 23:17:27 +0200

    feature product model , install http package

---
+++ lib/models/product_model.dart
@@ -0,0 +1,37 @@
+class ProductModel {
+  final int id;
+  final String title;
+  final double price;
+  final String description;
+  final String image;
+  final RatingModel rating;
+
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.image,
+    required this.rating,
+  });
+  factory ProductModel.fromJson(jsondata) {
+    return ProductModel(
+      id: jsondata['id'],
+      title: jsondata['title'],
+      price: jsondata['price'],
+      description: jsondata['description'],
+      image: jsondata['image'],
+      rating: RatingModel.fromJson(jsondata['rating']),
+    );
+  }
+}
+
+class RatingModel {
+  final double rate;
+  final int count;
+
+  RatingModel({required this.rate, required this.count});
+  factory RatingModel.fromJson(jsondata) {
+    return RatingModel(rate: jsondata['rate'], count: jsondata['count']);
+  }
+}
